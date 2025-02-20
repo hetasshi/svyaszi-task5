eb4d85dbd0330732dc9de5fe9f8d71971e6b367f
hetashi <101481527+hetasshi@users.noreply.github.com>
2025-02-20 11:21:06 +0300

    Update 1.cpp

--- praktik1/1.cpp
+++ praktik1/1.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
 
+class Engine {
+public:
+    void run() {
+        std::cout << "Двигатель запущен!" << std::endl;
+    }
+};
+
+class Car {
+private:
+    Engine engine;  
+
+public:
+    void start() {
+        std::cout << "Запуск автомобиля..." << std::endl;
+        engine.run();  
+    }
+};
+
+int main() {
+    setlocale(LC_ALL, "Rus");
+    Car myCar;
+    myCar.start();
+
+    return 0;
+}
